ab0b14ff0dab29a41b095b240f8969ea3de7b4a8
maathanghiganesh <maathanghiganesh@gmail.com>
2020-09-08 21:52:58 +0530

    To B

--- src/test/java/org/exa/Example.java
+++ src/test/java/org/exa/Example.java
@@ -1,5 +1,11 @@
 package org.exa;
 
 public class Example {
+	public static void main(String[] args) {
+		String s="hello";
+		String s1="hai";
+		System.out.println(s);
+		System.out.println(s1);
+	}
 
 }
